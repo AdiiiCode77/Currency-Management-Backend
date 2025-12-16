39d53c34328d6be6ab90c16219ef85cbbdcd7447
AdiiiCode <106529569+AdiiiCode77@users.noreply.github.com>
2025-12-16 11:21:41 +0500

    Refactor AdminEntity to use AdminType enum

--- src/modules/users/domain/entities/admin.entity.ts
+++ src/modules/users/domain/entities/admin.entity.ts
@@ -1,12 +1,23 @@
-
-import { adminTypes } from 'src/shared/enums/admin.enum';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum AdminType {
+  ADMIN = 'admin',
+  SUPERADMIN = 'superadmin',
+}
+
 @Entity('admins')
 export class AdminEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @Column({ type: 'text', enum: adminTypes, nullable: false })
-  type: string;
+
+  @Column({
+    type: 'enum',
+    enum: AdminType,
+    nullable: false,
+  })
+  type: AdminType;
+
   @Column({ type: 'uuid' })
   user_profile_id: string;
 }
+
