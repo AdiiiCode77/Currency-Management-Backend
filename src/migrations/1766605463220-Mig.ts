5bfd9c3454f55a1d4cb88aebd1093d5a7c9407a8
AdiiiCode <adeel2694551@gmail.com>
2025-12-25 00:45:11 +0500

    feat: add initial migration file for database schema changes

---
+++ src/migrations/1766605463220-Mig.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Mig1766605463220 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
